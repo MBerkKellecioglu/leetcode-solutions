03a8869421d05232a8851f1e2d8896cdc8b5e545
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2023-11-16 15:28:15 +0000

    Add LeetCode solution: 46 Permutations

---
+++ 46-permutations/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> sol;
+        vector<int> used_index(nums.size(), 0);
+        vector<int> sub_sol;
+
+        recPermute(nums, sol, sub_sol, used_index);
+
+        return sol;
+    }
+
+    void recPermute(vector<int>& nums, vector<vector<int>>& sol, vector<int>& sub_sol, vector<int>& used_index){
+        if(nums.size() == sub_sol.size()){
+            sol.push_back(sub_sol);
+            return;
+        }
+        else if(nums.size() < sub_sol.size()) return;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(!used_index[i]) sub_sol.push_back(nums[i]);
+            else continue;
+            used_index[i]++;
+            recPermute(nums, sol, sub_sol, used_index);
+            sub_sol.pop_back();
+            used_index[i]--;
+        }
+    }
+};
