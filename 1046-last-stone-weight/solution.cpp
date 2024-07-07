6445d8bc046aacf90e61151fed8c9df1c66c6f65
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2024-07-07 20:44:20 +0000

    Add LeetCode solution: 1046 Last Stone Weight

---
+++ 1046-last-stone-weight/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+
+        priority_queue<int> max_heap;
+
+        for(int i = 0; i < stones.size(); i++){
+            max_heap.push(stones[i]);
+        }
+
+        while(max_heap.size() > 1){
+            int y = max_heap.top();
+            max_heap.pop();
+
+            int x = max_heap.top();
+            max_heap.pop();
+
+            max_heap.push(y - x);
+        }
+
+        return max_heap.top();
+    }
+};
