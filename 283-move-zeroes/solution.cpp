f0a92259776e26c4c413c6647fa7b4d2d67489ea
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2024-10-21 22:26:50 +0000

    Add LeetCode solution: 283 Move Zeroes

---
+++ 283-move-zeroes/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int count = 0, n = nums.size();
+
+        for(int i = 0; i < n; i++){
+            if(!nums[i]) count++;
+            else{
+                nums[i - count] = nums[i];
+            }
+        }
+
+        for(int i = n - count; i < n; i++){
+            nums[i] = 0;
+        }
+
+    }
+};
