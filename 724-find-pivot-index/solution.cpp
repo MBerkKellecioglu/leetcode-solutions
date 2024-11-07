e9b5d43dd77404009febf03bd6915c11d6c837a0
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2024-11-07 10:36:35 +0000

    Add LeetCode solution: 724 Find Pivot Index

---
+++ 724-find-pivot-index/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        int sum = 0;
+
+        for(int& n : nums){
+            sum += n;
+        }
+
+        int l = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(l == sum - l - nums[i]) return i;
+
+            l += nums[i];
+        }
+
+        return -1; 
+    }
+};
