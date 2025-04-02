b4b566cc082a14cfb8d393cb0a11589eeb6d9cda
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2025-04-02 08:03:01 +0000

    Add LeetCode solution: 790 Domino and Tromino Tiling

---
+++ 790-domino-and-tromino-tiling/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+
+        if n <= 2:
+            return n
+        
+        dp = [0] * (n + 1)
+
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % (10**9 + 7)
+
+        return dp[n]
+            
+            
