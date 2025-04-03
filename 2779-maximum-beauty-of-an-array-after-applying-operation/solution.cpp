bb252ec1f6f8846c6d4a40ff9dbcfce8e4f9e5aa
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2025-04-03 20:31:49 +0000

    Add LeetCode solution: 2779 Maximum Beauty of an Array After Applying Operation

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k){
+
+        sort(nums.begin(), nums.end());
+
+        int ans = 1, l = 0;
+
+        for(int i = 1; i < nums.size(); i++){    
+            while(nums[i] - nums[l] > 2 * k) l++;
+            
+            ans = max(ans, i - l + 1);
+        }
+
+        return ans;
+    }
+};
