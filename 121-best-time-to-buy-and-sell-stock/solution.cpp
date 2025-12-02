43719f4a955b5e9b2f6c2fd882ffaff546e74d26
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2025-12-02 19:05:50 +0000

    Add LeetCode solution: 121 Best Time to Buy and Sell Stock

---
+++ 121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int buy = -prices[0], sell = 0;
+
+        for(int i = 1; i < prices.size(); i++){
+            buy = max(buy, -prices[i]);
+
+            sell = max(sell, buy + prices[i]);
+        }
+
+        return sell;
+    }
+};
