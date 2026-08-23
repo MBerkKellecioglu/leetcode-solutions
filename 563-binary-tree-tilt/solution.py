f8049bb15cc63df6ef3536338653e15272329eed
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2026-08-23 19:00:22 +0000

    Add LeetCode solution: 563 Binary Tree Tilt

---
+++ 563-binary-tree-tilt/solution.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        
+        total = 0
+
+        def dfs(node):
+            nonlocal total
+
+            if not node:
+                return 0
+
+            l = dfs(node.left)
+            r = dfs(node.right)
+
+            total += abs(l - r)
+
+            return l + r + node.val
+        
+        dfs(root)
+
+        return total
