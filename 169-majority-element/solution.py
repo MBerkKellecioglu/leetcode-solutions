e2f9e619b6c42dbbe9e6180db21b173c6530b1c4
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2026-09-14 05:40:02 +0000

    Add LeetCode solution: 169 Majority Element

---
+++ 169-majority-element/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        candidate = -1 # Boyer for the win
+        
+        count = 0
+        
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            else:
+                if candidate != num:
+                    count -= 1
+                else:
+                    count += 1
+        
+        return candidate
+        
