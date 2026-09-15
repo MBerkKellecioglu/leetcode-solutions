e795d1d8bb336d4c1af20d49190787aeb5294916
Mehmet Berk Kellecioglu <134450596+MBerkKellecioglu@users.noreply.github.com>
2026-09-15 03:08:08 +0000

    Add LeetCode solution: 509 Fibonacci Number

---
+++ 509-fibonacci-number/solution.py
@@ -0,0 +1,12 @@
+dp = [0] * 31
+
+dp[1] = 1
+
+for i in range(2,31):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        return dp[n]
+        
